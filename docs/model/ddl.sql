51fdde69f47e9d81ffe137d46fa8cdc62f2965f3
marcelobns <marcelobns@live.com>
2023-09-29 18:25:40 -0400

    adding seed for mccodes

--- docs/model/ddl.sql
+++ docs/model/ddl.sql
@@ -12,11 +12,13 @@ CREATE TABLE pre_registrations
 (
   id                bigint       NOT NULL GENERATED ALWAYS AS IDENTITY UNIQUE,
   registration_type varchar(100) NOT NULL,
-  document_number   varchar(20)  NOT NULL,
+  document_number   varchar(20)  NOT NULL UNIQUE,
   name              varchar(300) NOT NULL,
   email             varchar(300) NOT NULL UNIQUE,
-  attributes        json         NOT NULL,
   mc_code_id        bigint       NOT NULL,
+  attributes        text        ,
+  status            varchar(50)  DEFAULT 'queue',
+  observation       text        ,
   op                varchar(50)  NOT NULL,
   at                timestamp    NOT NULL DEFAULT current_timestamp,
   PRIMARY KEY (id)
@@ -24,18 +26,17 @@ CREATE TABLE pre_registrations
 
 COMMENT ON COLUMN pre_registrations.registration_type IS 'pessoa_fisica, pessoa_juridica';
 
+COMMENT ON COLUMN pre_registrations.status IS 'queue, open, finished';
+
 COMMENT ON COLUMN pre_registrations.op IS 'created, updated';
 
 CREATE TABLE prospects_queue
 (
   id                  bigint    NOT NULL GENERATED ALWAYS AS IDENTITY UNIQUE,
-  pre_registration_id bigint    NOT NULL,
+  pre_registration_id bigint    NOT NULL UNIQUE,
   queueing_at         timestamp NOT NULL DEFAULT current_timestamp,
-  op                  int       NOT NULL,
   PRIMARY KEY (id)
 );
-
-COMMENT ON COLUMN prospects_queue.op IS 'created, updated';
 
 ALTER TABLE prospects_queue
   ADD CONSTRAINT FK_pre_registrations_TO_prospects_queue
